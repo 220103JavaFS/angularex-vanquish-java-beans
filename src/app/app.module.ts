90e9b67b6c3baece44ab2531a87958e74640f9f8
Elias Calagiu <eliasc81@gmail.com>
2022-02-02 11:35:26 -0800

    elias component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,15 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { EricComponent } from './components/eric/eric.component';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
 import { EliasComponent } from './components/elias/elias.component';
-=======
 import { JustinComponent } from './components/justin/justin.component';
->>>>>>> 058b378fa5ff4009078386e5842960040fd021af
 
 @NgModule({
   declarations: [
     AppComponent,
     EricComponent,
-<<<<<<< HEAD
     EliasComponent
-=======
     JustinComponent
->>>>>>> 058b378fa5ff4009078386e5842960040fd021af
   ],
   imports: [
     BrowserModule,
