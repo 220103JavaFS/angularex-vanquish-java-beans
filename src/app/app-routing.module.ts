29f779b8f3530f19a6b5d2b90b8f951518c6d174
JustinBreaux <96553121+JustinBreaux@users.noreply.github.com>
2022-02-02 15:15:53 -0500

    component data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EricComponent } from './components/eric/eric.component';
+import { JustinComponent } from './components/justin/justin.component';
 
 const routes: Routes = [{
   path:"eric",
   component:EricComponent
+},{
+  path:"justin",
+  component:JustinComponent
 }];
 
 @NgModule({
