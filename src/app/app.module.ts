de3ce6754a97bf080f855dbb5f3fc8d9181f94f3
Jessie Guadarrama <jesguadarrama98@gmail.com>
2022-02-02 11:40:17 -0800

    solved conflicts pt2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,17 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { EricComponent } from './components/eric/eric.component';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
 import { JessieComponent } from './components/jessie/jessie.component';
-=======
 import { JustinComponent } from './components/justin/justin.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
->>>>>>> b3de7aa5164b23c8ac233dc679da72f144855a74
 
 @NgModule({
   declarations: [
     AppComponent,
     EricComponent,
-<<<<<<< HEAD
-    JessieComponent
-=======
+    JessieComponent,
     JustinComponent,
     NavbarComponent
->>>>>>> b3de7aa5164b23c8ac233dc679da72f144855a74
   ],
   imports: [
     BrowserModule,
